d45207c8c9470b8e84d3c5c55039523ad24e7313
Kurt Motekew <motekew@verizon.net>
2021-12-10 23:50:14 -0500

    moved Kepler1 to GCRF prop

--- src/astro_kepler.cpp
+++ src/astro_kepler.cpp
@@ -30,18 +30,12 @@ Kepler::Kepler(const JulianDate& epoch,
 {
   jd0 = epoch;
   ecfeci = ecfeciSys;
-  Eigen::Matrix<double, 6, 1> xecf = ecfeci->eci2ecf(jd0,
-                                                     xeci.block<3,1>(0,0),
-                                                     xeci.block<3,1>(3,0));
-  Eigen::Matrix<double, 6, 1> xteme = ecfeci->ecf2teme(jd0,
-                                                       xecf.block<3,1>(0,0),
-                                                       xecf.block<3,1>(3,0));
-  x0[0] = phy_const::km_per_du*xteme(0,0);
-  x0[1] = phy_const::km_per_du*xteme(1,0);
-  x0[2] = phy_const::km_per_du*xteme(2,0);
-  x0[3] = phy_const::km_per_du*xteme(3,0)*phy_const::tu_per_sec;
-  x0[4] = phy_const::km_per_du*xteme(4,0)*phy_const::tu_per_sec;
-  x0[5] = phy_const::km_per_du*xteme(5,0)*phy_const::tu_per_sec;
+  x0[0] = phy_const::km_per_du*xeci(0,0);
+  x0[1] = phy_const::km_per_du*xeci(1,0);
+  x0[2] = phy_const::km_per_du*xeci(2,0);
+  x0[3] = phy_const::km_per_du*xeci(3,0)*phy_const::tu_per_sec;
+  x0[4] = phy_const::km_per_du*xeci(4,0)*phy_const::tu_per_sec;
+  x0[5] = phy_const::km_per_du*xeci(5,0)*phy_const::tu_per_sec;
 }
 
 
@@ -53,24 +47,21 @@ Eigen::Matrix<double, 6, 1> Kepler::getStateVector(const JulianDate& jd,
   double t1 {cal_const::sec_per_day*(jd - jd0)};
   Kepler1(planet.data(), 0.0, x0.data(), t1, x1.data(), &x);
 
-  Eigen::Matrix<double, 6, 1> xteme;
-  xteme(0,0) = phy_const::du_per_km*x1[0];
-  xteme(1,0) = phy_const::du_per_km*x1[1];
-  xteme(2,0) = phy_const::du_per_km*x1[2];
-  xteme(3,0) = phy_const::du_per_km*x1[3]*phy_const::sec_per_tu;
-  xteme(4,0) = phy_const::du_per_km*x1[4]*phy_const::sec_per_tu;
-  xteme(5,0) = phy_const::du_per_km*x1[5]*phy_const::sec_per_tu;
-  Eigen::Matrix<double, 6, 1> xecf = ecfeci->teme2ecf(jd,
-                                                      xteme.block<3,1>(0,0),
-                                                      xteme.block<3,1>(3,0));
+  Eigen::Matrix<double, 6, 1> xeci;
+  xeci(0,0) = phy_const::du_per_km*x1[0];
+  xeci(1,0) = phy_const::du_per_km*x1[1];
+  xeci(2,0) = phy_const::du_per_km*x1[2];
+  xeci(3,0) = phy_const::du_per_km*x1[3]*phy_const::sec_per_tu;
+  xeci(4,0) = phy_const::du_per_km*x1[4]*phy_const::sec_per_tu;
+  xeci(5,0) = phy_const::du_per_km*x1[5]*phy_const::sec_per_tu;
 
-  if (frame == EphemFrame::eci) {
-    Eigen::Matrix<double, 6, 1> xeci = ecfeci->ecf2eci(jd,
-                                                       xecf.block<3,1>(0,0),
-                                                       xecf.block<3,1>(3,0));
-    return xeci;
+  if (frame == EphemFrame::ecf) {
+    Eigen::Matrix<double, 6, 1> xecf = ecfeci->eci2ecf(jd,
+                                                       xeci.block<3,1>(0,0),
+                                                       xeci.block<3,1>(3,0));
+    return xecf;
   }
-  return xecf;
+  return xeci;
 }
 
 
