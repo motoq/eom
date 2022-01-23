170d26100e9aa8caa58012b99d4c6a048c6d8024
Kurt Motekew <motekew@verizon.net>
2022-01-23 03:43:56 -0500

    adding grid points to test_ground_point

--- src/eom_test_ground_point.cpp
+++ src/eom_test_ground_point.cpp
@@ -7,6 +7,9 @@
  */
 
 #include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <string>
 
 #include <Eigen/Dense>
 
@@ -17,6 +20,7 @@
 #include <eom_test.h>
 
 static void test_xyz(Eigen::Matrix<double, 3, 1>&);
+static std::string fuk_start_str(eom::FukInit);
 
 namespace eom_app {
 
@@ -24,6 +28,8 @@ void eom_test_ground_point()
 {
 
   std::cout << "\n\n  === Test:  GroundPoint ===";
+
+    // Unit tests - init with Cartesian
 
   Eigen::Matrix<double, 3, 1> pos1P {6524.834, 6862.875, 6448.296};
   pos1P *= phy_const::du_per_km;
@@ -58,6 +64,43 @@ void eom_test_ground_point()
   test_xyz(posNp);
   std::cout << "    Near Polar Test";
 
+    // Test grid of lat/lon with random alt
+  
+  double max_lat_err {0.0};
+  double max_lon_err {0.0};
+  double max_alt_err {0.0};
+  int npts {0};
+  double lat {utl_const::pio2};
+  double dlat {-utl_const::pi/36.0};
+  while (lat >= -utl_const::pio2) {
+    double lon {utl_const::pi};
+    double dlon {-utl_const::pi/36.0};
+    while (lon >= -utl_const::pi) {
+      double alt {7.0};
+      double dalt {-0.1};
+      while (alt > -0.1) {
+        eom::GroundPoint gpLla {lat, lon, alt};
+        Eigen::Matrix<double, 3, 1> xyz = gpLla.getCartesian();
+        eom::GroundPoint gpXyz {xyz};
+        max_lat_err = std::max(max_lat_err,
+                               std::fabs(lat - gpXyz.getLatitude()));
+        max_lon_err = std::max(max_lon_err,
+                               std::fabs(lon - gpXyz.getLongitude()));
+        max_alt_err = std::max(max_alt_err,
+                               std::fabs(alt - gpXyz.getAltitude()));
+        alt += dalt;
+        npts++;
+      }
+      lon += dlon;
+    }
+    lat += dlat;
+  }
+  std::cout << "\nMax Error Over Grid of " << npts << " points" <<
+               "\n Lat: " << utl_const::deg_per_rad*max_lat_err << " (deg)" <<
+               "   Lon: " << utl_const::deg_per_rad*max_lon_err << " (deg)" <<
+               "   Alt: " << phy_const::m_per_du*max_alt_err << " m";
+
+
   std::cout << "\n  === End Test:  GroundPoint ===\n\n";
 }
 
@@ -69,8 +112,29 @@ static void test_xyz(Eigen::Matrix<double, 3, 1>& xyz)
 {
   eom::GroundPoint gp {xyz};
   std::cout << "\nnitr: " << gp.getItr();
+  std::cout << " using " << fuk_start_str(gp.getFukInit());
   eom::GroundPoint gp2(gp.getLatitude(), gp.getLongitude(), gp.getAltitude());
   std::cout << "  Error: " <<
       phy_const::m_per_du*(xyz - gp2.getCartesian()).norm() << " m";
   
 }
+
+
+static std::string fuk_start_str(eom::FukInit starter)
+{
+  switch (starter) {
+    case eom::FukInit::none:
+      return "None";
+    case eom::FukInit::case1:
+      return "Case1";
+    case eom::FukInit::case2:
+      return "Case2";
+    case eom::FukInit::case3a:
+      return "Case3a";
+    case eom::FukInit::case3b:
+      return "Case3b";
+    default:
+      return "";
+  }
+}
+
